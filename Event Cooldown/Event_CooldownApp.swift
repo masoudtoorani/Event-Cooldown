bec0180c54530be8f7468039505d988b2686cbac
masoudtoorani <masoud.t92@gmail.com>
2024-10-28 16:37:19 +0300

    Initial Commit

---
+++ Event Cooldown/Event_CooldownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_CooldownApp.swift
+//  Event Cooldown
+//
+//  Created by Masoud Toorani on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Event_CooldownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
